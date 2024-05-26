091cb2d27a42832de1d29462766c5e1910914448
codygeek01 <s474996633@gmail.com>
2024-05-26 23:31:52 +0530

    frrrr.....

--- backend/src/controllers/events.controller.ts
+++ backend/src/controllers/events.controller.ts
@@ -242,7 +242,7 @@ class EventsController extends AbstractController {
             options: string[];
           };
 
-          await this.ctx.eventPolls.create({
+          const eventPoll = await this.ctx.eventPolls.create({
             data: {
               heading,
               description,
@@ -255,7 +255,7 @@ class EventsController extends AbstractController {
           });
 
           for (const option of options) {
-            await this.ctx.eventPollOptions.createByPollId(id, option);
+            await this.ctx.eventPollOptions.createByPollId(eventPoll.id, option);
           }
 
           res.sendStatus(200);
